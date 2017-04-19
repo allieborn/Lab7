3175d85b1a27691bacb6c93fdf55809a82e9784e
Alexandra <aborn601722@Alexandras-MacBook-Pro.local>
2017-04-18 22:29:02 -0400

    comments add, catch added

--- src/com/InClassLabs/Main.java
+++ src/com/InClassLabs/Main.java
@@ -25,8 +25,10 @@ public class Main {
         String keepGoing = "yes";
 
 
+
+
         System.out.println("Welcome to Java, April 2017 class!: ");
-        do {
+        do { //do loop tests for if user wants to keep going
             System.out.println("Enter a student's number to find out more about them (1-14):");
 
             userSelection = scan.nextInt();
@@ -36,10 +38,12 @@ public class Main {
 
                 System.out.println("What would you like to know about " + studentName[userSelection - 1] + "? Enter 'favorite food' or 'favorite animal'.");
                 scan.nextLine();
+                //converts any user input entered to lower case
                 userWants = scan.nextLine().toLowerCase();
                 do {
 
                     if (userWants.contains("food")) {
+                        //adding the -1 here allows the number the user enters to directly correlate with the list of students (above)
                         System.out.println(studentName[userSelection - 1] + "'s favorite food is " + favFood[userSelection - 1] + ".");
                         break;
                     } else if (userWants.contains("animal")) {
@@ -49,22 +53,16 @@ public class Main {
                         System.out.println("You did not enter a valid selection. Please try again: ");
                         userWants = scan.nextLine().toLowerCase();
                     }
-                } while (userWants.contains("food") || (userWants.contains("animal")));
+                } while (userWants.contains("food") || (userWants.contains("animal"))); //allows user to enter only 'food' or 'animal' rather than re-type whole string
 
                 System.out.println("Would you like to learn about another student? Enter yes or no: ");
                 keepGoing = scan.nextLine().toLowerCase();
-            }  catch(IndexOutOfBoundsException e){
+            }  catch(IndexOutOfBoundsException e){ //catch refers back to top of try loop, which allows user to enter only 1-14. Any other number makes loop repeat
                 System.out.println("You did not enter a valid student number.");
             }
 
         } while (keepGoing.equalsIgnoreCase("yes"));
         System.out.println("Thanks for learning more about us!");
-        //NEED TO ADD CATCH EXCEPTION SO THAT IF ENTER INVALID STUDENT NUMBER IT STARTS OVER
-
     }
 }
 
-//if (heightParam < 0) {
-//        throw new Exception("Invalid height.");
-//        }
-//        return heightParam;
